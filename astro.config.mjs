33833589b12c9df69f16873a7332b18834f43fb7
Emiliano Uribe <iahrefs@gmail.com>
2025-08-03 16:04:45 -0600

    Agregué componentes nuevos y actualicé páginas del blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()]
+    site: "https://example.com",
+    integrations: [preact()]
 });
